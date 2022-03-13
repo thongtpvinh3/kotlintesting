d0b0bce7b9a6b36727841f5432e3d9c4d127751c
Nguyen Dinh Duy Thong <nguyendinhduythong@gmail.com>
2022-03-14 01:28:17 +0700

    redis config

--- src/main/kotlin/backend/kotlintesting/redisConfig/RedisConfiguration.kt
+++ src/main/kotlin/backend/kotlintesting/redisConfig/RedisConfiguration.kt
@@ -1,7 +1,26 @@
 package backend.kotlintesting.redisConfig
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
 
 @Configuration
 class RedisConfiguration {
+
+    @Bean
+    fun redisConnectionFactory(): LettuceConnectionFactory {
+        val config: RedisStandaloneConfiguration = RedisStandaloneConfiguration()
+        config.hostName = "localhost"
+        config.port = 6379
+        return LettuceConnectionFactory(config)
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any> {
+        val redisTemplate: RedisTemplate<String, Any> = RedisTemplate()
+        redisTemplate.setConnectionFactory(redisConnectionFactory())
+        return redisTemplate
+    }
 }
